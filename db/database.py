1435fe4de25b95f73ac931ed05e7f0379047aecd
John Doe <johndoe@email.com>
2024-07-06 17:01:08 +0500

    ssl change

--- db/database.py
+++ db/database.py
@@ -1,12 +1,13 @@
 import os
+import ssl
 
+import pymongo
 from dotenv import load_dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
 MONGODB_URL = os.getenv("MONGODB_URL")
-client = AsyncIOMotorClient(MONGODB_URL)
+client = pymongo.MongoClient(MONGODB_URL, ssl_cert_reqs=ssl.CERT_NONE)
 db = client.db
 users_collection = db.users
 audio_files_collection = db.audio_files
