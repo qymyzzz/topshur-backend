b6f1bbca290d3d8879638c6a482d6ea020c5dac7
John Doe <johndoe@email.com>
2024-07-06 17:08:02 +0500

    ssl update another

--- db/database.py
+++ db/database.py
@@ -1,13 +1,14 @@
 import os
-import ssl
 
+import certifi
 import pymongo
 from dotenv import load_dotenv
 
 load_dotenv()
 
+ca = certifi.where()
 MONGODB_URL = os.getenv("MONGODB_URL")
-client = pymongo.MongoClient(MONGODB_URL, ssl_cert_reqs=ssl.CERT_NONE)
+client = pymongo.MongoClient(MONGODB_URL, tlsCAFile=ca)
 db = client.db
 users_collection = db.users
 audio_files_collection = db.audio_files
